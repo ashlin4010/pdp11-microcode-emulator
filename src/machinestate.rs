315a0696dc97e53b9dd0a3d105abb0d68d5bd136
unknown <ashlin.inwood@gmail.com>
2025-02-15 19:16:39 +1300

    Added BUT MUX and BUT(SWITCH)

--- src/machinestate.rs
+++ src/machinestate.rs
@@ -1,6 +1,6 @@
 #![allow(non_snake_case)]
 
-use crate::{clock::ClockState, microrom::MicroWord};
+use crate::{clock::{ClockState, CL1}, microrom::{MicroWord, MICROROM}};
 
 pub struct MachineState<'a> {
 
@@ -8,28 +8,47 @@ pub struct MachineState<'a> {
     // contains associated information
     pub CLK_MODE: &'a ClockState,
 
+    // U WORD Register - Comprises multiple chips
+    // At the beginning of the current machine state a ROM microword is clocked into the U WORD Register
+    pub U_WORD: &'a MicroWord,
 
     // Microprogram Pointer - 8 bits + 1 (K2-2)
     // Initially we'll leave UPP8 unimplemented
     pub UPP: u8,
 
-
     // Buffered Microprogram Pointer - 8 bits
     // Essentially the same as the UPP, used by the diagnostic tool
     pub BUPP: u8,
-
 
     // Past Microprogram Pointer - 8 bits (K2-2)
     // UPP is stored in the PUPP, the PUPP Register is clocked each time the microword is clocked
     pub PUPP: u8,
 
-
     // Basic Microbranch Control - 5 bits
-    // This is a collection of latching multiplexers
+    // BUBC represents the logical OR of the inputs into the
+    // OR gates that feed the UPP latches excluding the UPP
+    // Both BUBC0 and lines have previously been ORed
     pub BUBC: u8,
+    pub BUBC_FLUX: u8,
+
+    // SWITCH Flip-Flip
+    // Set on: ADRS_SW, EXAM_SW, CONT_SW, DEP_SW, START and BEGIN
+    // Cleared on: not sure
+    pub SWITCH: bool
+}
 
 
-    // U WORD Register - Comprises multiple chips
-    // At the beginning of the current machine state a ROM microword is clocked into the U WORD Register
-    pub U_WORD: &'a MicroWord
+impl MachineState<'_> {
+    pub fn new() -> MachineState<'static> {
+        MachineState {
+            CLK_MODE: &CL1,
+            U_WORD: &MICROROM[0],
+            UPP: 0,
+            BUPP: 0,
+            PUPP: 0,
+            BUBC: 0,
+            BUBC_FLUX: 0,
+            SWITCH: false,
+        }
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -18,16 +18,93 @@ const STARTING_UPP: u8 = 0;
 const MACHINE_CYCLE_SLEEP: Duration = time::Duration::from_millis(100);
 
 
-fn evaluate_but(flag: bool) -> u8 {
-    if flag {
-        0x01
-    } else {
-        0        
-    }
-}
+fn evaluate_bubc(state: &MachineState) -> u8 {
 
-fn set_but(but: u8) -> bool {
-    but == 0o6
+    /*
+     * BUBC consists of:
+     * BUBC0 - BUT 17:00 (74LS150)
+     * BUBC0 - BUT 37:20 (74LS150)
+     * BUBC1 - (74LS158)
+     * BUBC2 - (74LS151)
+     * BUBC3 - (74LS151)
+     * BUBC4 - (74LS153)
+     * BUBC5 - (74LS153)
+     */
+
+    let UBF = state.U_WORD.UBF;
+    let UBF_4 = UBF >> 4 & 1;
+    let UBF_0_1_2_3 = UBF & 0b1111; // Used a mux input
+
+    let mut BUBC: u8 = 0;
+    let mut BUBC0: u8 = 0;
+    let mut BUBC1: u8 = 0;
+
+    if UBF_4 == 0 {
+        // E97
+        BUBC0 = match UBF_0_1_2_3 {
+            0b0000 => 0,                // 0 NOOP
+            0b0001 => todo!("E97 1"),   // 1
+            0b0010 => todo!("E97 2"),   // 2
+            0b0011 => todo!("E97 3"),   // 3
+            0b0100 => todo!("E97 4"),   // 4
+            0b0101 => todo!("E97 5"),   // 5
+            0b0110 => state.SWITCH as u8, // 6 BUT(SWITCH)
+            0b0111 => todo!("E97 7"),   // 7
+            0b1000 => todo!("E97 8"),   // 8
+            0b1001 => todo!("E97 9"),   // 9
+            0b1010 => todo!("E97 10"),  // 10
+            0b1011 => todo!("E97 11"),  // 11
+            0b1100 => todo!("E97 12"),  // 12
+            0b1101 => todo!("E97 13"),  // 13
+            0b1110 => todo!("E97 14"),  // 14
+            0b1111 => todo!("E97 15"),  // 15 BUT(IR03)
+            _ => panic!("Invalid UBF mux on E97")
+        };
+    } else {
+        
+        BUBC0 = match UBF_0_1_2_3 { // E81
+            0b0000 => todo!("E81 0"),   // 0 BUT(20)
+            0b0001 => todo!("E81 1"),   // 1
+            0b0010 => todo!("E81 2"),   // 2
+            0b0011 => panic!("E81 3 - Not Used"),   // 3
+            0b0100 => 0,                // 4
+            0b0101 => todo!("E81 5"),   // 5
+            0b0110 => todo!("E81 6"),   // 6
+            0b0111 => todo!("E81 7"),   // 7
+            0b1000 => todo!("E81 8"),   // 8
+            0b1001 => todo!("E81 9"),   // 9
+            0b1010 => todo!("E81 10"),  // 10
+            0b1011 => todo!("E81 11"),  // 11
+            0b1100 => todo!("E81 12"),  // 12
+            0b1101 => todo!("E81 13"),  // 13
+            0b1110 => todo!("E81 14"),  // 14
+            0b1111 => todo!("E81 15"),  // 15
+            _ => panic!("Invalid UBF mux on E81")
+        };
+
+        BUBC1 = match UBF_0_1_2_3 { // E72
+            0b0000 => todo!("E72 0"),   // 0
+            0b0001 => todo!("E72 1"),   // 1
+            0b0010 => todo!("E72 2"),   // 2
+            0b0011 => panic!("E72 3 - Not Used"),   // 3
+            0b0100 => todo!("E72 2"),   // 4
+            0b0101 => todo!("E72 5"),   // 5
+            0b0110 => todo!("E72 6"),   // 6
+            0b0111 => todo!("E72 7"),   // 7
+            0b1000 => todo!("E72 8"),   // 8
+            0b1001 => todo!("E72 9"),   // 9
+            0b1010 => todo!("E72 10"),  // 10
+            0b1011 => todo!("E72 11"),  // 11
+            0b1100 => todo!("E72 12"),  // 12
+            0b1101 => todo!("E72 13"),  // 13
+            0b1110 => todo!("E72 14"),  // 14
+            0b1111 => todo!("E72 15"),  // 15
+            _ => panic!("Invalid UBF mux on E72")
+        };
+
+    }
+
+    return BUBC | BUBC0 | (BUBC1 << 1);
 }
 
 fn main() {
@@ -35,17 +112,10 @@ fn main() {
     //-------------------------------
     //---------Machine State---------
     //-------------------------------
-    let mut MACHINE_STATE = MachineState {
-        CLK_MODE: &CL1,
-        UPP: STARTING_UPP,
-        BUPP: 0,
-        PUPP: 0,
-        BUBC: 0,
-        U_WORD: &MICROROM[STARTING_UPP as usize],
-    };
+    let mut MACHINE_STATE = MachineState::new();
+    MACHINE_STATE.U_WORD = &MICROROM[STARTING_UPP as usize];
 
-    let mut switch_flag = false;
-
+    MACHINE_STATE.SWITCH = true; // Fake a key press
 
     //---------------------------------
     //--------Application Loop---------
@@ -53,7 +123,6 @@ fn main() {
     loop {
         // Set up next machine cycle
         print!("{esc}[2J{esc}[1;1H", esc = 27 as char); // Clear the screen
-
 
         println!("New Machine Cycle");
         println!("Current UPP = {:o}", MACHINE_STATE.UPP);
@@ -79,21 +148,7 @@ fn main() {
             }
 
             // For the next 140ms to 300ms the cpu will do stuff
-            // Set BUT MUX
-            // GET value from last but mux
-            switch_flag = set_but(MACHINE_STATE.U_WORD.UBF);
-
-
             println!("Current UBF = {:o}", MACHINE_STATE.U_WORD.UBF);
-
-            // if P1 || (P2 && clock_mode.mode != ClockMode::CL3) || P3 {
-            //     // Demo: on microstate 26 evaluate BUT(switch)
-            //     if (UPP == 0o46) {
-            //         BUBC = 0x01;
-            //     } else {
-            //         BUBC = 0x00; // no switch
-            //     }
-            // }
 
 
             // At the end of the machine cycle latch the UWORD from the ROM
@@ -109,12 +164,13 @@ fn main() {
 
                 // Clock so latch in new microword
                 MACHINE_STATE.U_WORD = &MICROROM[MACHINE_STATE.UPP as usize];
-
                 
-                MACHINE_STATE.BUBC = evaluate_but(switch_flag); // update BUBC
+                // The BUBC lines are in flux for one machine cycle, we save that state in BUBC_NEXT
+                MACHINE_STATE.BUBC = MACHINE_STATE.BUBC_FLUX;
+                // evaluate_bubc must be called at the end of the machine cycle to allow time for BUTs to settle
+                MACHINE_STATE.BUBC_FLUX = evaluate_bubc(&MACHINE_STATE);
 
-                // Compute next UPP based off the content of BUBC from the last machine cycle
-                // next_UPP = U_WORD.UPF | BUBC;
+                // TODO: Clear switch
             }
         }
 
@@ -142,7 +198,6 @@ fn main() {
         println!("\nThe values below are that will be used at the start of the next machine cycle");
         println!("UPP = {:o}", MACHINE_STATE.UPP);
         println!("CLK = {:?}", MACHINE_STATE.CLK_MODE.mode);
-
 
         // Alow time for the users to watch the simulator
         match SINGLE_STEP {
